e93c7e2bae8f6f90038631d42c640f8080f019a2
Vitor Marinho <vitor@uw-s.nl>
2024-11-27 22:40:39 +0100

    web>app>page cleaned

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,22 +1,7 @@
-import Image, { type ImageProps } from "next/image";
+import Image from "next/image";
 import { Button } from "@repo/ui/components/ui/button";
 import styles from "./page.module.css";
 
-type Props = Omit<ImageProps, "src"> & {
-  srcLight: string;
-  srcDark: string;
-};
-
-const ThemeImage = (props: Props) => {
-  const { srcLight, srcDark, ...rest } = props;
-
-  return (
-    <>
-      <Image {...rest} src={srcLight} className="imgLight" />
-      <Image {...rest} src={srcDark} className="imgDark" />
-    </>
-  );
-};
 
 export default function Home() {
   return (
